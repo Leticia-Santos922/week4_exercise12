ef12bb12303148863d8569fcaf6676a5e1fbc14f
leticia <leticia.nuness.santos@gmail.com>
2024-02-11 19:29:51 +0000

    completed comments added practice file for projects

--- ex12_quest1.py
+++ ex12_quest1.py
@@ -1,29 +1,26 @@
 # obj: Understand the syntax for tuples, lists and dictionaries
 
 # TODO: what is wrong with the below code
-
+# lists:
+# ordered collections of objects (also known as sequences)
+# objects containing a sequential collection of other objects called elements
+# lists are mutable where they extend or shrink - new items can be added on or removed
 # list can be iterable
 cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
 # python interpret 'oke' string as an iterable (means you can iterate over the characters)
-# cheese += 'Oke'  # each character of oke as an individual item to the list
+# cheese += 'Oke'  # each character of oke is concatenated as an individual item to the list
 
-# a) how should oke be added to the end of the list?
+# A) how should oke be added to the end of the list?
 # compound logic operations (research)
-# += is a shorthand assignment operator which is used to concatenate (add) to sequences (list)
-cheese += ['Oke']  # needed the [] brackets as added entire list to the end of the cheese to add Oke
+# += is a shorthand assignment operator which is used to append (add) an element to sequences (list)
+cheese += ['Oke']  # needed the [] brackets to be an element and added to the end of the cheese list to add Oke
 print(cheese)
-
-# can be used to add more than one value
-# cheese += ['Oke', 'Leicester']
-# print(cheese)
-
-
-# TODO: adding an item at the beginning of a list (things to look after the homework)
-# TODO: adding an item in the middle of the list (things to look at after the homework)
-
 
 # b) how would you add two new cheese with a single command?
 
-print(f"added to cheese list: {cheese + ['Oke', 'Leceister']}")
+# Used + arithmatic operator concatenates the existing list cheese with the new list with two new cheese elements
+# f-string formats the output message and uses {} and interpolates the expression cheese to concatenate to new list
+# the print function stringifies the concatenated lists and message
+print(f"added two new cheeses to the list: {cheese + ['Oke', 'Leceister']}")
 
 # extend method Equivalent to my_list.extend([4, 5])
